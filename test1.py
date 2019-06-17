1bb7d96d77da8b447a78427c03d771a99c1b2105
lawn <bloom.all.over.12@gmail.com>
2019-06-17 19:56:43 +0900

    test1を10秒毎にループし動作を確認する

--- test1.py
+++ test1.py
@@ -1,27 +1,32 @@
 from auth import exampleAuth
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
+import time
 
-accountID, token = exampleAuth()
-client = API(access_token=token)
+while True:
 
-# APIで１分足を100件取得
-instrument = "GBP_JPY"
-params = {
-    "count": 100,
-    "granularity": "M1"
-}
-r = instruments.InstrumentsCandles(instrument=instrument, params=params)
-response = client.request(r)
+    accountID, token = exampleAuth()
+    client = API(access_token=token)
 
-# 最後から２番目のローソク足を取り出す
-data = response["candles"][-2]
+    # APIで１分足を100件取得
+    instrument = "GBP_JPY"
+    params = {
+        "count": 100,
+        "granularity": "M1"
+    }
+    r = instruments.InstrumentsCandles(instrument=instrument, params=params)
+    response = client.request(r)
 
-# ローソク足から日時・始値・終値を取り出す
-close_time = data["time"]
-open_price = data["mid"]["o"]
-close_price = data["mid"]["c"]
+    # 最後から２番目のローソク足を取り出す
+    data = response["candles"][-2]
 
-print("時間： " + close_time
-      + " 始値： " + str(open_price)
-      + " 終値： " + str(close_price))
+    # ローソク足から日時・始値・終値を取り出す
+    close_time = data["time"]
+    open_price = data["mid"]["o"]
+    close_price = data["mid"]["c"]
+
+    print("時間： " + close_time
+          + " 始値： " + str(open_price)
+          + " 終値： " + str(close_price))
+
+    time.sleep(10)

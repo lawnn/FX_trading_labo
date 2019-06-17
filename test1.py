be6f9e4bce1fbf773f0557448994c6140f50b900
lawn <bloom.all.over.12@gmail.com>
2019-06-17 18:19:43 +0900

    ポンド円の1分足を100件取得 その中から最後から２番目のローソク足を取り出す そこから日時、始値、終値を取り出すテスト

---
+++ test1.py
@@ -0,0 +1,27 @@
+from auth import exampleAuth
+from oandapyV20 import API
+import oandapyV20.endpoints.instruments as instruments
+
+accountID, token = exampleAuth()
+client = API(access_token=token)
+
+# APIで１分足を100件取得
+instrument = "GBP_JPY"
+params = {
+    "count": 100,
+    "granularity": "M1"
+}
+r = instruments.InstrumentsCandles(instrument=instrument, params=params)
+response = client.request(r)
+
+# 最後から２番目のローソク足を取り出す
+data = response["candles"][-2]
+
+# ローソク足から日時・始値・終値を取り出す
+close_time = data["time"]
+open_price = data["mid"]["o"]
+close_price = data["mid"]["c"]
+
+print("時間： " + close_time
+      + " 始値： " + str(open_price)
+      + " 終値： " + str(close_price))

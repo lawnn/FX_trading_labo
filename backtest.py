308196b914df0bcf0d88969991b1bf66354a212c
lawn <bloom.all.over.12@gmail.com>
2019-06-26 05:13:59 +0900

    fileからの読み込みテスト

--- backtest.py
+++ backtest.py
@@ -3,6 +3,7 @@ from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
 import dateutil.parser
 import time
+import pandas as pd
 
 
 def get_price(response, i):
@@ -14,6 +15,16 @@ def get_price(response, i):
             "high_price": float(last_data["mid"]['h']),
             "low_price": float(last_data["mid"]['l']),
             "close_price": float(last_data["mid"]['c'])}
+
+
+def get_price_from_file(path, i):
+    last_data = pd.read_csv(path)
+
+    return {"close_time": last_data.loc[i, 'time'],
+            "open_price": round(last_data.loc[i, 'o'], 3),
+            "high_price": round(last_data.loc[i, 'h'], 3),
+            "low_price": round(last_data.loc[i, 'l'], 3),
+            "close_price": round(last_data.loc[i, 'c'], 3)}
 
 
 def print_price(data):
@@ -54,8 +65,9 @@ client = API(access_token=token)
 instrument = "GBP_JPY"
 params = {
     "count": 5000,
-    "granularity": "M1"
+    "granularity": "H1"
 }
+path = 'csv/' + instrument + '_' + params['granularity'] + '_' + '2017.1.1' + '.csv'
 r = instruments.InstrumentsCandles(instrument=instrument, params=params)
 response = client.request(r)
 i = 0
@@ -64,7 +76,7 @@ print_price(last_data)
 flag = 0
 
 while i < 5000:
-    data = get_price(response, i)
+    data = get_price_from_file(path, i)
     if data["close_time"] != last_data["close_time"]:
         print_price(data)
         if flag == 0 and check_candle(data):
